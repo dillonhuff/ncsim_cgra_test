a80b245059c120f037b2669a7b04eefd92e2ea82
Dillon Bailey Huff <dhuff@kiwi.Stanford.EDU>
2018-03-28 18:11:18 -0700

    Am now loading in configurations to the CGRA. Not sure if the config is actually being read though

--- test.v
+++ test.v
@@ -11,6 +11,8 @@ module test();
    
    wire tck;
 
+   reg reset_done;
+   
    reg [31:0] config_addr;
    reg [31:0] config_data;
 
@@ -19,7 +21,8 @@ module test();
    
    initial begin
 
-      config_file = $fopen("../../CGRAGenerator/bitstream/examples/conv_1_2.bs", "r");
+      config_file = $fopen("../../CGRAGenerator/bitstream/examples/pw2_sixteen.bsa", "r");
+      reset_done = 0;
 
       if (config_file == 0) begin
 	 $display("config_file was null");
@@ -37,39 +40,47 @@ module test();
       #2 rst = 1;
       #3 rst = 0;
 
+      reset_done = 1;
       $display("DONE WITH RESET");
-
-      #3 clk = ~clk;
-      #3 clk = ~clk;
-      #3 clk = ~clk;
-      #3 clk = ~clk;
 
    end // initial begin
 
    always #2 clk = ~clk;
 
-   always @(posedge clk) begin
+   reg [0:0] data_in_S0_T0_reg;
+   
+   // After reseting load data / configuration between rising clock edges
+   always @(negedge clk) begin
 
       $display("clk = %d", clk);
       $display("config file = %d", config_file);
 
-      scan_file = $fscanf(config_file, "%h %h\n", config_addr, config_data);
+      if (reset_done) begin
+	 scan_file = $fscanf(config_file, "%h %h\n", config_addr, config_data);
 
-      if (!$feof(config_file)) begin
+	 if (!$feof(config_file)) begin
 	 
-	 $display("config addr = %h", config_addr);
-	 $display("config data = %h", config_data);
+	    $display("config addr = %h", config_addr);
+	    $display("config data = %h", config_data);
 	 
-      end else begin
-	 $display("Reached end of file!");
+	 end else begin
+	    $display("Reached end of file!");
+	    data_in_S0_T0_reg <= 1'b1;
+	 end
       end
-
    end
 
+
+
+   wire [0:0] data_in_S0_T0;
+   assign data_in_S0_T0 = data_in_S0_T0_reg;
+   
    top cgra(.clk_in(clk),
 	    .reset_in(rst),
 	    .config_addr_in(config_addr),
 	    .config_data_in(config_data),
+
+	    .pad_S0_T0(data_in_S0_T0),
 	    
 	    .tdi(tdi),
 	    .tms(tms),
