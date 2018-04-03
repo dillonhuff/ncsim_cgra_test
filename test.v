3a7324fc819864bffb8239b792ed0192b837bad4
Dillon Bailey Huff <dhuff@kiwi.Stanford.EDU>
2018-04-03 15:24:50 -0700

    Adding real inputs and outputs to CGRA

--- test.v
+++ test.v
@@ -65,22 +65,104 @@ module test();
 	 
 	 end else begin
 	    $display("Reached end of file!");
-	    data_in_S0_T0_reg <= 1'b1;
 	 end
       end
    end
 
+   wire [0:0] data_out_S0_T0;
+   wire [0:0] data_out_S0_T1;
+   wire [0:0] data_out_S0_T2;
+   wire [0:0] data_out_S0_T3;
+   wire [0:0] data_out_S0_T4;
+   wire [0:0] data_out_S0_T5;
+   wire [0:0] data_out_S0_T6;
+   wire [0:0] data_out_S0_T7;
+   wire [0:0] data_out_S0_T8;
+   wire [0:0] data_out_S0_T9;
+   wire [0:0] data_out_S0_T10;
+   wire [0:0] data_out_S0_T11;
+   wire [0:0] data_out_S0_T12;
+   wire [0:0] data_out_S0_T13;
+   wire [0:0] data_out_S0_T14;
+   wire [0:0] data_out_S0_T15;
 
+   always @(posedge clk) begin
+      $display("data_out_S0_T15 = %d", data_out_S0_T15);
+   end
 
-   wire [0:0] data_in_S0_T0;
-   assign data_in_S0_T0 = data_in_S0_T0_reg;
+   
+   wire [0:0] data_in_S2_T0;
+   wire [0:0] data_in_S2_T1;
+   wire [0:0] data_in_S2_T2;
+   wire [0:0] data_in_S2_T3;
+   wire [0:0] data_in_S2_T4;
+   wire [0:0] data_in_S2_T5;
+   wire [0:0] data_in_S2_T6;
+   wire [0:0] data_in_S2_T7;
+   wire [0:0] data_in_S2_T8;
+   wire [0:0] data_in_S2_T9;
+   wire [0:0] data_in_S2_T10;
+   wire [0:0] data_in_S2_T11;
+   wire [0:0] data_in_S2_T12;
+   wire [0:0] data_in_S2_T13;
+   wire [0:0] data_in_S2_T14;
+   wire [0:0] data_in_S2_T15;   
+
+   assign data_in_S2_T0 = 1'h1;
+   assign data_in_S2_T1 = 1'h1;
+   assign data_in_S2_T2 = 1'h1;
+   assign data_in_S2_T3 = 1'h1;
+   assign data_in_S2_T4 = 1'h1;
+   assign data_in_S2_T5 = 1'h1;
+   assign data_in_S2_T6 = 1'h1;
+   assign data_in_S2_T7 = 1'h1;
+   assign data_in_S2_T8 = 1'h1;
+   assign data_in_S2_T9 = 1'h1;
+   assign data_in_S2_T10 = 1'h1;
+   assign data_in_S2_T11 = 1'h1;
+   assign data_in_S2_T12 = 1'h1;
+   assign data_in_S2_T13 = 1'h1;
+   assign data_in_S2_T14 = 1'h1;
+   assign data_in_S2_T15 = 1'h1;
    
    top cgra(.clk_in(clk),
 	    .reset_in(rst),
 	    .config_addr_in(config_addr),
 	    .config_data_in(config_data),
 
-	    .pad_S0_T0(data_in_S0_T0),
+	    .pad_S2_T0_in(data_in_S2_T0),
+	    .pad_S2_T1_in(data_in_S2_T1),
+	    .pad_S2_T2_in(data_in_S2_T2),
+	    .pad_S2_T3_in(data_in_S2_T3),
+	    .pad_S2_T4_in(data_in_S2_T4),
+	    .pad_S2_T5_in(data_in_S2_T5),
+	    .pad_S2_T6_in(data_in_S2_T6),
+	    .pad_S2_T7_in(data_in_S2_T7),
+	    .pad_S2_T8_in(data_in_S2_T8),
+	    .pad_S2_T9_in(data_in_S2_T9),
+	    .pad_S2_T10_in(data_in_S2_T10),
+	    .pad_S2_T11_in(data_in_S2_T11),
+	    .pad_S2_T12_in(data_in_S2_T12),
+	    .pad_S2_T13_in(data_in_S2_T13),
+	    .pad_S2_T14_in(data_in_S2_T14),
+	    .pad_S2_T15_in(data_in_S2_T15),
+
+	    .pad_S0_T0_out(data_out_S0_T0),
+	    .pad_S0_T1_out(data_out_S0_T1),
+	    .pad_S0_T2_out(data_out_S0_T2),
+	    .pad_S0_T3_out(data_out_S0_T3),	    	    
+	    .pad_S0_T4_out(data_out_S0_T4),
+	    .pad_S0_T5_out(data_out_S0_T5),
+	    .pad_S0_T6_out(data_out_S0_T6),
+	    .pad_S0_T7_out(data_out_S0_T7),
+	    .pad_S0_T8_out(data_out_S0_T8),
+	    .pad_S0_T9_out(data_out_S0_T9),
+	    .pad_S0_T10_out(data_out_S0_T10),
+	    .pad_S0_T11_out(data_out_S0_T11),
+	    .pad_S0_T12_out(data_out_S0_T12),
+	    .pad_S0_T13_out(data_out_S0_T13),
+	    .pad_S0_T14_out(data_out_S0_T14),
+	    .pad_S0_T15_out(data_out_S0_T15),	    
 	    
 	    .tdi(tdi),
 	    .tms(tms),
