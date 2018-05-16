3843c485916e3741770d1fe434f470f98f7be352
Dillon Bailey Huff <dhuff@kiwi.Stanford.EDU>
2018-05-16 14:32:05 -0700

    Added cgra verilog to repo

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 
-run_cmd = 'irun -status -top test -timescale 1ns/1ns -l irun.log -access +rwc -notimingchecks -input cmd.tcl ../../CGRAGenerator/hardware/generator_z/top/genesis_verif/*.v ../../CGRAGenerator/hardware/generator_z/top/genesis_verif/*.sv ./test.v'
+run_cmd = 'irun -status -top test -timescale 1ns/1ns -l irun.log -access +rwc -notimingchecks -input cmd.tcl ./cgra_dev_no_mdll_05_15_2018/*.v ./cgra_dev_no_mdll_05_15_2018/*.sv ./test.v'
 
 #run_cmd = 'irun -top test -timescale 1ns/1ns -l irun.log -access -rwc -notimingchecks -input cmd.tcl ../CGRA_coreir/cgra_with_config_ports/*.v ../CGRA_coreir/cgra_with_config_ports/*.sv ./test.v'
 
